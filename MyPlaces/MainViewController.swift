607195728827ea299cdb286176628a7a5a49cf3e
Vladimir Dvornikov <echobizarre@gmail.com>
2022-06-22 00:41:41 +0300

    Added icon Launch screen Row height

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -15,38 +15,39 @@ class MainViewController: UITableViewController {
         "Lucky Noodles", "Лора Крафт", "Старик и море",
         "Рамен Изакая бар"
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 80
     }
-
+    
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return restaurantNames.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         var content = cell.defaultContentConfiguration()
         
         content.text = restaurantNames[indexPath.row]
         content.image = UIImage(named: restaurantNames[indexPath.row])
+        //content.imageProperties.cornerRadius = tableView.rowHeight / 2
         
         cell.contentConfiguration = content
         return cell
     }
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
